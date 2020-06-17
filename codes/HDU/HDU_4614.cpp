3c8d10d219264a5fc5c4c7c36bdd562bf3cc4441
willy108 <weimingchess108@gmail.com>
2020-06-17 12:11:46 -0700

    HDU 4614 is a work in progress. must close temporarily

--- codes/HDU/HDU_4614.cpp
+++ codes/HDU/HDU_4614.cpp
@@ -14,6 +14,7 @@
 #include <list>
 #include <queue>
 #include <stack>
+#include <deque>
 //#include <unordered_map>
 //#include <unordered_set>
 #include <functional>
@@ -38,7 +39,8 @@ void setIO(const string& file_name){
 int set_tag[max_v], add[max_v];
 long long sum[max_v];
 bool marked[max_v];
-  
+pair<int, int> arr[220000];  
+int siz[max_v];
 class seg_tree{
   int s, n;
   //d is min, t is max
@@ -85,6 +87,18 @@ class seg_tree{
     int mid = (L + R) / 2;
     return Q_sum(qL, qR, LC(k), L, mid) + Q_sum(qL, qR, RC(k), mid, R);
   }
+  
+  void G(int qL, int qR, int k, int L, int R, deque<int>& dq){
+    if(R <= L || R <= qL || qR <= L) return;
+    if(qL <= L && R <= qR){
+      dq.push_back(k);
+      return ;
+    }
+    //push_down(k, L, R);
+    int mid = (L + R) / 2;
+    G(qL, qR, LC(k), L, mid, dq);
+    G(qL, qR, RC(k), mid, R, dq);
+  }
 
   public:
 
@@ -94,6 +108,14 @@ class seg_tree{
       memset(sum, 0, sizeof(sum));
       memset(set_tag, 0, sizeof(set_tag));
       memset(marked, false, sizeof(marked));
+      for(int i = n - 1; i < n*2; i++){
+        arr[i] = make_pair(i - (n - 1), i - (n - 2));
+        siz[i] = 1;
+      }
+      for(int i = n - 2; i >= 0; i--){
+        arr[i] = make_pair(arr[LC(i)].first, arr[RC(i)].second);
+        siz[i] = siz[LC(i)] * 2;
+      }
     }
 
 
@@ -105,19 +127,112 @@ class seg_tree{
       return Q_sum(l, r, 0, 0, this->n);
     }
     
+    int query_sum(int k){
+      return Q_sum(arr[k].first, arr[k].second, 0, 0, this->n);
+    }
+
+    deque<int> get_intervals(int l, int r){
+      deque<int> dq;
+      G(l, r, 0, 0, this->n, dq);
+      return dq;
+    }
+
     void print(){
       for(int i = 0; i<(this->n - 1) + this->s; i++){
-        printf("%d:\n\tlc->%d, rc->%d\n\tsum->%lld\n\tset_tag->%d\n", i, LC(i), RC(i), sum[i], set_tag[i]);
+        printf("%d:\n\tlc->%d, rc->%d\n\tleft_bound->%d, right_bound->%d\n\tsum->%lld\n\tmarked->%d, set_tag->%d\n", i, LC(i), RC(i), arr[i].first, arr[i].second, sum[i],(int) marked[i], set_tag[i]);
       }
       puts("");
     }
-
+    
+    void P(){
+      for(int i = 0; i<n - 1; i++){
+       // printf("arr[%d] == %lld\n",i, query_sum(i, i + 1));
+      }
+      puts("");
+    }
 };
+
+seg_tree S(4);
+
+int left_0(const deque<int>& DQ){
+  deque<int> dq = DQ;
+  while(!dq.empty()){
+    int cur = dq.front(); dq.pop_front();
+//    printf("%d, %d %d\n", cur, arr[cur].first, arr[cur].second);
+    if(S.query_sum(cur) == siz[cur]) cont;
+    else if(siz[cur] == 1 && S.query_sum(cur) == 0) return arr[cur].first;
+    else dq.push_front(RC(cur)), dq.push_front(LC(cur));
+  }
+  return -1;
+}
+
+int DIE(const deque<int>& DQ){
+  deque<int> dq = DQ;
+  while(!dq.empty()){
+    int cur = dq.back(); dq.pop_back();
+    if(S.query_sum(cur) == siz[cur]) cont;
+    else if(siz[cur] == 1 && S.query_sum(cur) == 0) return arr[cur].first;
+    else dq.push_back(LC(cur)), dq.push_back(RC(cur));
+  }
+  return -1;
+}
+
+int right_0(const deque<int>& DQ, int b){
+  int tot = 0;
+  deque<int> dq = DQ;
+  while(!dq.empty()){
+    int cur = dq.front(); dq.pop_front();
+ //   printf("%d, %d %d %d %lld\n", cur, arr[cur].first, arr[cur].second, siz[cur], sum[cur]);
+    if(tot + siz[cur] - (int)S.query_sum(cur) < b) tot += siz[cur] -(int)S.query_sum(cur);
+    else if(tot + 1 == b && S.query_sum(cur) == 0LL && siz[cur] == 1) return arr[cur].first;
+    else dq.push_front(RC(cur)), dq.push_front(LC(cur));
+  }
+  return DIE(DQ);
+}
 
 
 int main(){
-  
-  
+  int T;
+  scanf("%d", &T);
+  while(T--){
+    int n, q;
+    scanf("%d%d", &n, &q);
+    S = seg_tree(n);
+    S.set_val(0, n, 0);
+   // S.print();
+    while(q--){
+      int t, a, b;
+      scanf("%d%d%d", &t, &a, &b);
+      //S.print();
+      if(t == 1){
+        deque<int> temp = S.get_intervals(a, n);
+       // for(int X : temp)
+       //   printf("%d, %d %d\n", X, arr[X].first, arr[X].second);
+       // puts("");
+        int L = left_0(temp);// puts("");
+        int R = right_0(temp, b);// puts("");
+       // assert((L != -1) ^ (R != -1));
+        if(L == -1) printf("Can not take any one.\n");
+        else{
+          //if(R < 0) R = right_0(temp, R * -1);
+          assert(R >= 0);
+          printf("%d %d\n", L, R);
+         // S.P();
+          S.set_val(L, R + 1, 1);
+         // S.P();
+        }
+
+      }else{
+        b++;
+        printf("%lld\n", S.query_sum(a, b));
+       // S.P();
+        S.set_val(a, b, 0);
+        //S.P();
+      }
+    }
+    
+    puts("");
+  }
 
 	return 0;
 }
