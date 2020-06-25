b225c721f4fd1f7161dc4a2fadf7c262a556c532
willy108 <weimingchess108@gmail.com>
2020-06-25 16:40:01 -0700

    utilies 2

--- codes/CF/CF_787E.cpp
+++ codes/CF/CF_787E.cpp
@@ -18,7 +18,7 @@
 //#include <unordered_set>
 #include <functional>
 
-#define max_v 1100
+#define max_v 210000
 #define LOGN 50
 #define int_max 0x3f3f3f3f
 #define cont continue
@@ -49,86 +49,53 @@ void dup(int& k){
   k = ind;
 }
 
-int get_ind(int key){
-  return lower_bound(srt, srt + n, key) - &srt[0];
-} 
-
-void U(int qL, int qR, int& k, int val, int L, int R){
-  if(R <= L || qR <= L || R <= qL) return ;
+void U(int p, int val, int& k, int L, int R){
+  if(R <= L || p < L || R <= p) return ;
   dup(k);
-  if(qL <= L && R <= qR){
-    add[k] += val;
+  if(L + 1 == R){
+    sum[k] += val;
     return ;
   }
 
   int mid = (L + R) / 2;
-  U(qL, qR, lc[k], val, L, mid);
-  U(qL, qR, rc[k], val, mid, R);
-  sum[k] = sum[lc[k]] + sum[rc[k]] + add[lc[k]] + add[rc[k]];
+  U(p, val, lc[k], L, mid);
+  U(p, val, rc[k], mid, R);
+  sum[k] = sum[lc[k]] + sum[rc[k]];
 }
 
-int Q(int qL, int qR, int k, int L, int R){
-  if(R <= L || qR <= L || R <= qL) return 0;
-  if(qL <= L && R <= qR) return sum[k] + add[k];
-  int mid = (L + R) / 2;
-  return Q(qL, qR, lc[k], L, mid) + Q(qL, qR, rc[k], mid, R) + (min(R, qR) - max(L, qL)) * add[k];
-}
 
 void pre_process(){
   s = pow_2(LOG2(n));
   ind = s*2;
   root[0] = 0;
-  for(int i = 0; i<s - 1; i++){
+  for(int i = 0; i<s; i++){
     lc[i] = LC(i);
     rc[i] = RC(i);
-    srt[i] = arr[i];
+    occ[i] = -1;
   }
-  sort(srt, srt + n);
-  
-  for(int i = 1; i<=n; i++){
-    root[i] = root[i - 1];
-    int ind = get_ind(arr[i - 1]);
-    assert(occ[ind] < i);
-    U(occ[ind], i, root[i], 1, 0, s);
-    occ[ind] = i;
+    
+  for(int i = n; i; i++){
+    int a = arr[i - 1];
+    if(occ[a] == -1){
+      U(i, 1, root[i], 0, S);
+    }else{
+      int temp = root[i + 1];
+      U(occ[a], -1, temp , 0, s);
+      root[i] = temp;
+      U(i, 1, root[i], 0, s);
+    }
+    occ[a] = i;
   }
-  
-}
-
-int count(int a, int b){ //0-based, exclusive
-  return Q(a, a + 1, root[b], 0, s);
-}
-
-int bin_search(int L, int l, int r, int k){//returns the EXCLUSIVE right end of the largest interval that has at most k colors
-  printf("%d %d %d %d %d %d\n", k, L, l, (l + r)/2, r, count(L, (l + r)/2));
-  if(l >= r) return l;
-  int mid = (l + r) / 2;
-   if(k >= count(L, mid)){
-    return bin_search(L, mid + 1, r, k);
-  }else{
-    return bin_search(L, l, mid, k);
-  }
-  
 
 }
-
-
 
 int main(){
-	scanf("%d", &n);
-  for(int i = 0; i<n; i++){
-    scanf("%d", &arr[i]);    
-  }
-  pre_process();
-  for(int i = 1; i<=n; i++){
-    int L = 0, tot = 0;
-    while(L < n){
-      L = bin_search(L, L + 1, n, i);
-      tot++;
-    }
-    printf("%d ", tot);
-  } 
+ 
+  scanf("%d", &n);
 
+  for(int i = 0; i<n; i++){
+    scanf("%d", &n);
+  }
 
 	return 0;
 }
