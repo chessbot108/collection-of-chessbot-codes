ab1ace2d9050b9464da8b929459e6fc7a626c7f7
chessbot108 <weimingchess108@gmail.com>
2020-07-24 22:47:11 -0700

    preparing for test. had tomas do board game

--- codes/TC_MOCK/board_game.cpp
+++ codes/TC_MOCK/board_game.cpp
@@ -1,54 +1,28 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-#include <cassert>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <map>
-#include <set>
-#include <sstream>
-#include <list>
-#include <queue>
-#include <stack>
-//#include <unordered_map>
-//#include <unordered_set>
-#include <functional>
-
-#define max_v 110000 // i actually have no clue what N is
-#define LOGN 50
-#define int_max 0x3f3f3f3f
-#define cont continue
-#define byte_max 0x3f
-#define pow_2(n) (1 << (n))
-//tree
-#define lsb(n) ((n)&(-(n)))
-#define LC(n) (((n) << 1) + 1)
-#define RC(n) (((n) << 1) + 2)
-#define LOG2(n) ((int)(ceil(log2((n)))))
+#include <bits/stdc++.h>
 using namespace std;
 
-void setIO(const string& file_name){
-	freopen((file_name+".in").c_str(), "r", stdin);
-	freopen((file_name+".out").c_str(), "w+", stdout);
-}
-
-int dep[max_v], arr[max_v];
-
-
-
-int main(){
+int main() {
   int n;
-  scanf("%d", &n);
-  for(int i = 0; i<n; i++){
-    scanf("%d", &arr[i]);
+  cin >> n;
+  int arr[n];
+  bool vis[n + 1];
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
   }
-  
-
-
-	return 0;
+  int ind = 1;
+  int steps = 0;
+  bool fail = false;
+  while(true){
+    vis[ind] = true;
+    if(arr[ind - 1] == 0) break;
+    if(!vis[arr[ind - 1]]){
+      ind = arr[ind - 1];
+      steps++;
+    } else {
+      fail = true;
+      break;
+    }
+  }
+  if(fail) cout << -1 << endl;
+  else cout << steps << endl;
 }
-
