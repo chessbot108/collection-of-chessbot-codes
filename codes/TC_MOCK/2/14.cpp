412fa067baf7ebd13405dd6b8a5f46fffe6d7223
chessbot108 <weimingchess108@gmail.com>
2020-07-26 22:40:19 -0700

    debugging 14

--- codes/TC_MOCK/2/14.cpp
+++ codes/TC_MOCK/2/14.cpp
@@ -43,7 +43,8 @@ int BIT[max_v], inv[max_v];
 void U(int k, int v){
   if(k >= 55000) return ;
   BIT[k] += v;
-  U(k + lsb(k));
+  assert(k);
+  U(k + lsb(k), v);
 }
 
 int S(int k){
@@ -53,42 +54,65 @@ int S(int k){
 
 void cdq(int l, int r){//[a, b) as i always do 
   if(l + 1 >= r) return ;
-  int mid = (L + R) / 2;
-  cdq(L, mid);
-  cdq(mid, R);
+  printf("%d %d\n", l, r);
+  int mid = (l + r) / 2;
+  cdq(l, mid);
+  cdq(mid, r);
   for(int i = l; i<r; i++){
-    if(ins[arr[i]]) U(ins[arr[i]], -1); //best way i could come up with the clear the bit...
+    if(ins[arr[i]]){
+      printf("ins[arr[%d]] where arr[i] = %d, is %d\n", i, arr[i], ins[arr[i]]);
+      U(ins[arr[i]], -1);
+    }//best way i could come up with the clear the bit...
   }
-  int i = l, j = mid;
+  int i = l, j = mid, k = l;
   /**
   this part is bad for my brain.
   i am so glad it is unique keys
   counting i's inversions: S(ins[arr[i]]); //amount indexed left and lesser value and lesser time
   counting j's inversions: S*ins[arr[j]]); but sort backwards
   **/
-  
+  //assert(false); 
+  printf("now merging\n");
   while(true){
-    if(j >= r || i < mid(srt[i] < srt[j])){
-      inv[srt[i]] = S(ins[srt[i]]);
-      i++;
-    }else{
-      j++;
+    printf("%d\n", l);
+    if(j >= r || (i < mid && arr[i] < arr[j])){
+      inv[arr[i]] += S(ins[arr[i]]);
+      srt[l++] = arr[i++];
+      printf("\tgoes to i, is adding\n");
+    }else if(j < r){
+      printf("\tgoes to j, is updating %d which the timestamp of %d\n", ins[arr[j]], arr[j]);
+      U(ins[arr[j]], 1);
+      srt[l++] = arr[j++];
     }
     if(i >= mid && j >= r){
       assert(i == mid && j == r);
       break;
     }
   }
-  
+  //assert(false); 
   for(int i = l; i<r; i++){
     if(ins[arr[i]]) U(ins[arr[i]], -1); //best way i could come up with the clear the bit...
   }
-  
+  j = r - 1, i = mid - 1, l = r - 1;
   while(true){
-    if(
-  }
-  
+    if(j < mid || (i >= l && arr[i] > arr[j])){
+      printf("%d, %d %d\n", i, arr[i], ins[arr[i]]);
+      U(ins[arr[i]], 1);
 
+      unsrt[l--] = arr[i--];
+    }else{
+      inv[arr[j]] += S(ins[arr[j]]);
+      unsrt[l--] = arr[j--];
+    }
+    if(i < l && j < mid){
+      assert(i == l - 1 && j == mid - 1);
+      break;
+    }
+  }
+
+  for(int i = l; i<r; i++){
+    arr[i] = srt[i];
+  }
 
 }
 
@@ -99,14 +123,14 @@ int main(){
   scanf("%d%d", &n, &m);
   for(int i = 0; i<n; i++){
     scanf("%d", &arr[i]);
-    ins[i] = 1;
+    ins[i + 1] = 1;
   }
 
   for(int i = 0; i<m; i++){
     scanf("%d", &rem[i]);
-    ins[rem[i]] = i + 1;
+    ins[rem[i]] = i + 2;
   }   
-  
+  cdq(0, n);   
   return 0;
 }
 
