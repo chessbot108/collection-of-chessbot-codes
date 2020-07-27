37dd56e374d4d51c6cb3e4cdfe5f482653f46c4e
chessbot108 <weimingchess108@gmail.com>
2020-07-26 20:29:48 -0700

    14 is progress

--- codes/TC_MOCK/2/14.cpp
+++ codes/TC_MOCK/2/14.cpp
@@ -36,12 +36,13 @@ void setIO(const string& file_name){
 	freopen((file_name+".out").c_str(), "w+", stdout);
 }
 
-int rem[max_v], BIT[max_v], arr[max_v], inv[max_v];
 int n, m;
+int rem[max_v], arr[max_v], ins[max_v], unsrt[max_v], srt[max_v];
+int BIT[max_v], inv[max_v];
 
-void U(int k){
-  if(k >= max_v) return ;
-  BIT[k]++;
+void U(int k, int v){
+  if(k >= 55000) return ;
+  BIT[k] += v;
   U(k + lsb(k));
 }
 
@@ -50,10 +51,62 @@ int S(int k){
   return BIT[k] + S(k - lsb(k));
 }
 
-int main(){
+void cdq(int l, int r){//[a, b) as i always do 
+  if(l + 1 >= r) return ;
+  int mid = (L + R) / 2;
+  cdq(L, mid);
+  cdq(mid, R);
+  for(int i = l; i<r; i++){
+    if(ins[arr[i]]) U(ins[arr[i]], -1); //best way i could come up with the clear the bit...
+  }
+  int i = l, j = mid;
+  /**
+  this part is bad for my brain.
+  i am so glad it is unique keys
+  counting i's inversions: S(ins[arr[i]]); //amount indexed left and lesser value and lesser time
+  counting j's inversions: S*ins[arr[j]]); but sort backwards
+  **/
+  
+  while(true){
+    if(j >= r || i < mid(srt[i] < srt[j])){
+      inv[srt[i]] = S(ins[srt[i]]);
+      i++;
+    }else{
+      j++;
+    }
+    if(i >= mid && j >= r){
+      assert(i == mid && j == r);
+      break;
+    }
+  }
+  
+  for(int i = l; i<r; i++){
+    if(ins[arr[i]]) U(ins[arr[i]], -1); //best way i could come up with the clear the bit...
+  }
+  
+  while(true){
+    if(
+  }
+  
 
+
+}
+
+
+
+
+int main(){
+  scanf("%d%d", &n, &m);
+  for(int i = 0; i<n; i++){
+    scanf("%d", &arr[i]);
+    ins[i] = 1;
+  }
+
+  for(int i = 0; i<m; i++){
+    scanf("%d", &rem[i]);
+    ins[rem[i]] = i + 1;
+  }   
   
   return 0;
 }
-
 
