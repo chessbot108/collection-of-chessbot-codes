c8a4aa80bcfe3c00fa0358f9c54072365bf12d2b
chessbot108 <weimingchess108@gmail.com>
2020-07-27 21:40:21 -0700

    14 still working

--- codes/TC_MOCK/2/14.cpp
+++ codes/TC_MOCK/2/14.cpp
@@ -38,10 +38,10 @@ void setIO(const string& file_name){
 
 int n, m;
 int rem[max_v], arr[max_v], ins[max_v], unsrt[max_v], srt[max_v];
-int BIT[max_v], inv[max_v];
+int BIT[50010], inv[max_v], ans[max_v];
 
 void U(int k, int v){
-  if(k >= 55000) return ;
+  if(k >= 50010) return ;
   BIT[k] += v;
   assert(k);
   U(k + lsb(k), v);
@@ -54,55 +54,40 @@ int S(int k){
 
 void cdq(int l, int r){//[a, b) as i always do 
   if(l + 1 >= r) return ;
-  printf("%d %d\n", l, r);
   int mid = (l + r) / 2;
   cdq(l, mid);
   cdq(mid, r);
-  for(int i = l; i<r; i++){
-    if(ins[arr[i]]){
-      printf("ins[arr[%d]] where arr[i] = %d, is %d\n", i, arr[i], ins[arr[i]]);
-      U(ins[arr[i]], -1);
-    }//best way i could come up with the clear the bit...
-  }
+  memset(BIT, 0, sizeof(BIT));
   int i = l, j = mid, k = l;
   /**
   this part is bad for my brain.
   i am so glad it is unique keys
-  counting i's inversions: S(ins[arr[i]]); //amount indexed left and lesser value and lesser time
-  counting j's inversions: S*ins[arr[j]]); but sort backwards
+  counting i's inversions: S(BIT2, ins[arr[i]]); //amount indexed left and lesser value and lesser time
+  counting j's inversions: S(BIT1, ins[arr[j]]);
   **/
-  //assert(false); 
-  printf("now merging\n");
   while(true){
-    printf("%d\n", l);
-    if(j >= r || (i < mid && arr[i] < arr[j])){
+    //printf("%d\n", l);
+    if((j >= r || arr[i] < arr[j]) && i < mid){
       inv[arr[i]] += S(ins[arr[i]]);
-      srt[l++] = arr[i++];
-      printf("\tgoes to i, is adding\n");
+      srt[k++] = arr[i++];
     }else if(j < r){
-      printf("\tgoes to j, is updating %d which the timestamp of %d\n", ins[arr[j]], arr[j]);
       U(ins[arr[j]], 1);
-      srt[l++] = arr[j++];
+      srt[k++] = arr[j++];
     }
     if(i >= mid && j >= r){
       assert(i == mid && j == r);
       break;
     }
   }
-  //assert(false); 
-  for(int i = l; i<r; i++){
-    if(ins[arr[i]]) U(ins[arr[i]], -1); //best way i could come up with the clear the bit...
-  }
-  j = r - 1, i = mid - 1, l = r - 1;
+  memset(BIT, 0, sizeof(BIT));
+  j = r - 1, i = mid - 1, k = r - 1;
   while(true){
-    if(j < mid || (i >= l && arr[i] > arr[j])){
-      printf("%d, %d %d\n", i, arr[i], ins[arr[i]]);
+    if((j < mid || arr[i] > arr[j]) && i >= l){
       U(ins[arr[i]], 1);
-
-      unsrt[l--] = arr[i--];
-    }else{
+      unsrt[k--] = arr[i--];
+    }else if(j >= mid){
       inv[arr[j]] += S(ins[arr[j]]);
-      unsrt[l--] = arr[j--];
+      unsrt[k--] = arr[j--];
     }
     if(i < l && j < mid){
       assert(i == l - 1 && j == mid - 1);
@@ -128,9 +113,26 @@ int main(){
 
   for(int i = 0; i<m; i++){
     scanf("%d", &rem[i]);
-    ins[rem[i]] = i + 2;
+    ins[rem[i]] = (m + 1) - i;
   }   
-  cdq(0, n);   
+  cdq(0, n);  
+  int total = 0;
+  for(int i = 1; i<=n; i++){
+    if(ins[i] == 1) total += inv[i];
+  }
+  total /= 2;
+  for(int i = m - 1; ~i; i--){
+    ans[i] = (total += inv[rem[i]]);
+  }
+  /** 
+  for(int i = 1; i<=n; i++){
+    printf("%d: %d %d\n", i, inv[i], ins[i]);
+  }
+  **/
+  for(int i = 0; i<m; i++){
+    printf("%d\n", ans[i]);
+  }
+
   return 0;
 }
 
