d106a219b713f49017f149380df84342f7cc6627
chessbot108 <66888352+chessbot108@users.noreply.github.com>
2020-11-12 21:52:43 -0800

    Update CSES_1687.cpp

--- codes/CSES/CSES_1687.cpp
+++ codes/CSES/CSES_1687.cpp
@@ -6,31 +6,31 @@
 #include <algorithm>
 #include <vector>
 #include <utility>
-#define max_v 220000
-#define SQRT 500
+#define max_v 220000 
+#define SQRT 500 //n * SQRT is around 1e8. light constant factor lol
 #define cont continue
 using namespace std;
 int par[max_v], super[max_v]; //super stores the "super" parent or the parent SQRT layers up
 int n, q; //sqrt decomp for the win
 int main(){
-  scanf("%d%d", &n, &q);
+  scanf("%d%d", &n, &q); 
   for(int i = 2; i<=n; i++){
-    scanf("%d", &par[i]);
+    scanf("%d", &par[i]); //this made everything else so much easier.
   }
   
   for(int i = 1; i<=n; i++){
     super[i] = i;
-    for(int j = 0; j<SQRT && super[i]; j++) super[i] = par[super[i]];
-  }
+    for(int j = 0; j<SQRT && super[i]; j++) super[i] = par[super[i]]; //sqrt decomp so f*cking op
+  } //no edge cases. and all "trivial" looping. better than sparse table. dunno abt euler tour. maybe euler tour can O(1)
 
   while(q--){
     int u, k;
     scanf("%d%d", &u, &k);
     for(; k >= SQRT; k -= SQRT){
-      u = super[u];
+      u = super[u]; //at most SQRT possible super parents to take
     }
-    while(k--) u = par[u];
-    printf("%d\n", u ? u : -1);
+    while(k--) u = par[u]; //also at most SQRT possible nodes to iterate manually
+    printf("%d\n", u ? u : -1); //AC frist try lol
   }
   return 0;
 }
