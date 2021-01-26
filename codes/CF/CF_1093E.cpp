86ff9c240c94e4575f76112fca09c0d57c997656
wayming108 <55268829+wayming108@users.noreply.github.com>
2021-01-25 21:33:46 -0800

    I GOT INTERSECTIONS OF PERMUTATIONS AC. YAYYYYY!!!!!! thank dnq and thanks the cows. i have 2 failed versions too

--- codes/CF/CF_1093E.cpp
+++ codes/CF/CF_1093E.cpp
@@ -1,108 +1,100 @@
+//that took way too long. ac with dnq. woot woot
+//this problem can be seen as the 3d partial order problem
+//as you have to count the updates with time, x, and y
+//before the current query. this is solvable with cdq dnq
+//big thanks to this blog
+//https://robert1003.github.io/2020/01/31/cdq-divide-and-conquer.html
+
 #include <iostream>
 #include <queue>
 #include <cassert>
 #include <algorithm>
 #include <cmath>
-#define max_v 500000
+#include <vector>
 
+#define max_v 510000
 #define cont continue
 #define lsb(a) ((a) & -(a))
 using namespace std;
-const int SQRT = 450;
-int BIT[SQRT][max_v];
+
+struct query{
+    int op, x, y, ind, amt, val;
+    query(){}
+    query(int a, int b, int c, int d, int e, int f){ op=a,x=b,y=c,ind=d,amt=e,val=f;}
+} arr[max_v * 8], tmp[max_v * 8];
+
 int A[max_v], B[max_v], C[max_v], D[max_v];
-int n, m;
-int S(int k, int rt){
-    int ans = 0;
-    for(k++; k; k -= k&-k) ans += BIT[rt][k];
-    return ans;
-}
+int ans[max_v], rec[max_v], BIT[max_v], vec[max_v], sz = 0, h = 0, n, m;
 
-void U(int rt, int k, int val){
-    for(k++; k <= n; k += k&-k) BIT[rt][k] += val;
-}
+int S(int k){ return (!k) ? 0 : BIT[k] + S(k - lsb(k));}
+void U(int k, int val){ for(; k<=n && k; k += lsb(k)) BIT[k] += val;}
 
-int S(int l, int r, int h){
-    int rt_l = l/SQRT +1, rt_r = r/SQRT, ans = 0;
-    printf("%d %d %d %d %d", l, r, rt_l, rt_r, h); cout << endl;
-    if(rt_l >= rt_r){
-        while(l != r){ 
-            cout << l << " " << r << " " << C[B[l]] << " " << (C[B[l]] <= h) << endl;
-            ans += (C[B[l++]] <= h);
+void solve(int l, int r){
+    if(l + 1 == r) return ;
+    int mid = (l + r)/2;
+    solve(l, mid); solve(mid, r);
+    int j = l, k = mid, sum = 0, cnt = 0;
+    while(j < mid || k < r){
+        if(k == r || (j < mid && arr[j].x <= arr[k].x)){
+            U(arr[j].y, arr[j].val);
+            rec[sum++] = arr[j].y * arr[j].val;
+            tmp[cnt++] = arr[j++];
+        }else{      
+            ans[arr[k].ind] += arr[k].amt * S(arr[k].y);
+            tmp[cnt++] = arr[k++];
         }
-        return ans;
     }
-
-    while(l != rt_l * SQRT){
-        ans += (C[B[l++]] <= h);
+    
+    assert(cnt == r - l);
+    for(int i = 0; i<sum; i++){
+        if(rec[i] < 0) U(-rec[i], 1); else U(rec[i], -1);
+        rec[i] = 0;
     }
-    while(r != rt_r * SQRT){
-        ans += (C[B[--r]] <= h);
-    }
-    while(rt_l != rt_r){
-        ans += S(h, rt_l);
-        rt_l += SQRT;
-    }
-    return ans;
-}
-
-void U(int x, int y){
-    int a = C[B[x]], b = C[B[y]];
-    U(x/SQRT, a, -1);
-    U(y/SQRT, b, -1);
-    U(x/SQRT, b, 1);
-    U(y/SQRT, a, 1);
-    swap(B[x], B[y]);
-    D[B[x]] = x;
-    D[B[y]] = y;
-}
-
-void print(){
-    for(int i = 0; i<=n; i++){
-        for(int j = 0; j<=n; j++){
-            cout << S(i, i + 1, j) << " ";
-        }
-        cout << endl;
-    }
-    for(int i = 0; i<n; i++){
-        cout << C[B[i]] << endl;
+    assert(!S(n + 1)); //make sure the BIT is 0
+    for(int i = l; i<r; i++){
+        arr[i] = tmp[i - l];
     }
 }
+
+void mk(int a, int b, int c, int d, int e, int f){ arr[sz++] = query(a, b, c, d, e, f); }
 
 int main(){
-    
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(0);
+    cin.tie(0) -> sync_with_stdio(0);
     cin >> n >> m;
-    for(int i = 0; i<n; i++){
+    for(int i = 1; i<=n; i++){
         cin >> A[i];
         C[A[i]] = i;
     }
-    for(int i = 0; i<n; i++){
+    for(int i = 1; i<=n; i++){
         cin >> B[i];
-        D[B[i]] = i;
     }
 
-    for(int i = 0; i<n; i++){
-        U(i/SQRT, C[B[i]]);
+    for(int i = 1; i<=n; i++){
+        mk(2, C[B[i]], i, 0, 0, 1);
     }
-    int op, a, b, c, d;
-    //print();
-    for(int i = 0; i<n; i++){
-        printf("%d %d %d %d %d", i, A[i], B[i], C[B[i]], D[A[i]]); cout << endl;
-    }
-    while(m--){
-        cin >> op;
+    for(int i = 1; i<=m; i++){
+        int op, a, b, c, d; cin >> op;
         if(op - 1){
-            cin >> a >> b; a--, b--;
-            U(a, b);
+            cin >> a >> b;
+            mk(op, C[B[a]], a, 0, 0, -1);
+            mk(op, C[B[b]], b, 0, 0, -1);
+            swap(B[a], B[b]);
+            mk(op, C[B[a]], a, 0, 0, 1);
+            mk(op, C[B[b]], b, 0, 0, 1);
         }else{
-            cin >> a >> b >> c >> d; a--, c--, d--;
-            cout << S(a, b, d) << " " << S(a, b, c - 1) << endl;
-            cout << S(a, b, d) - S(a, b, c - 1) << endl;
+            cin >> a >> c >> b >> d;
+            a--, b--;
+            mk(op, a, b, h, 1, 0);
+            mk(op, c, d, h, 1, 0);
+            mk(op, a, d, h, -1, 0);
+            mk(op, c, b, h, -1, 0);
+            h++;
         }
-        //print();
     }
 
+    solve(0, sz);
+    for(int i = 0; i<h; i++){
+        cout << ans[i] << endl;
+    }
     return 0;
 }
