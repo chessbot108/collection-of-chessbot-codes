e68d1427a9b0689d59095dd77815cc5083a2a431
chessbot108 <weimingchess108@gmail.com>
2021-02-18 16:47:51 -0800

    added skeleton.cpp, my c++ template

---
+++ codes/templates/skeleton.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+
+#define max_v 1100
+#define int_max 0x3f3f3f3f
+#define cont continue
+#define pow_2(n) (1 << (n))
+#define ll long long
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define lsb(n) ((n)&(-(n)))
+#define LC(n) (((n) << 1) + 1)
+#define RC(n) (((n) << 1) + 2)
+#define add(a, b) ((a%mod + b%mod)%mod)
+#define mul(a, b) ((a%mod * b%mod)%mod)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+
+const ll mod = 1e9 + 7;
+
+using namespace std;
+
+void setIO(const string& file_name){
+	freopen((file_name+".in").c_str(), "r", stdin);
+	freopen((file_name+".out").c_str(), "w+", stdout);
+}
+
+
+int main(){
+	cin.tie(0) -> sync_with_stdio(0);
+
+	return 0;
+}
+
