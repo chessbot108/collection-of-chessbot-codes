545a28816b730bf0c1db9430091882e44e5945f1
chessbot108 <weimingchess108@gmail.com>
2021-03-07 18:24:17 -0800

    solved holes. appealed to cnblogs

--- codes/CF/CF_13E.cpp
+++ codes/CF/CF_13E.cpp
@@ -1,3 +1,5 @@
+//right off a cnblogs
+//my approach was also prolly correct
 #include <iostream>
 #include <cstdio>
 #include <cstring>
@@ -7,10 +9,11 @@
 #include <cassert>
 #include <algorithm>
 #include <vector>
+#include <random>
+#include <chrono>
 
-#define int_max 0x3f3f3f3f
 #define cont continue
-#define pow_2(n) (1 << (n))
+#define pow2(n) (1 << (n))
 #define ll long long
 #define pii pair<int, int>
 #define pb push_back
@@ -18,92 +21,75 @@
 #define lsb(n) ((n)&(-(n)))
 #define LC(n) (((n) << 1) + 1)
 #define RC(n) (((n) << 1) + 2)
-#define add(a, b) ((a%mod + b%mod)%mod)
-#define mul(a, b) ((a%mod * b%mod)%mod)
+#define add(a, b) (((a)%mod + (b)%mod)%mod)
+#define mul(a, b) (((a)%mod * (b)%mod)%mod)
 #define init(arr, val) memset(arr, val, sizeof(arr))
 
 const ll mod = 1e9 + 7;
-const int SQRT = 320, max_v = 1e5 +10;
+const int MX = 1e5 +10, int_max = 0x3f3f3f3f;
+const int SQRT = 320;
 using namespace std;
 
 void setIO(const string& file_name){
 	freopen((file_name+".in").c_str(), "r", stdin);
 	freopen((file_name+".out").c_str(), "w+", stdout);
 }
-//a 2700 nani?
-//batch processing
-//going to use a union find idea to guarantee O(n root n)
-//wish me luck
 
-struct query{
-  int u, val, op;
-  query(){}
-  query(int a, int b){op = a, u = b;}
-  query(int a, int b, int c){op = a, u = b, val = c;}
-} todo[max_v];
+int arr[MX], b[MX], jumps[MX], par[MX], lpos[MX];
+int n, q;
 
-int par[max_v], arr[max_v], jumps[max_v], vis[max_v];
-int qu[max_v]; //you qu (chinese for interesting) also happened to be short for query :O
-int n, q, ind = 0;
-
-
-void recompute(){
-  for(int i = n; i>=1; i--){
-    if(i + arr[i] > n){
-      jumps[i] = 1;
-    }else if(qu[i]){
-      jumps[i] = 1;
-      par[i] = arr[i] + i;
-    }else{
-      if(qu[i + arr[i]]){
-        
-      }
-    }
-  }
-  init(qu, 0);
+int up(int i){
+  return i*SQRT;
 }
 
-void Q(int u){
-  int tot = 0;
-  while(u + arr[u] <= n){
-    tot += jumps[u];
-    u = par[u];
+int down(int i){
+  return (i*SQRT) - SQRT +1;
+}
+
+void recompute(int l, int r){
+  r = min(r, n);
+  for(int i = r; i >= l; i--){
+    int nex = i + arr[i];
+    if(nex > n || nex > up(b[i])){
+      jumps[i] = 1;
+      par[i] = nex;
+      lpos[i] = i;
+    }else{
+      jumps[i] = jumps[nex] +1;
+      par[i] = par[nex];
+      lpos[i] = lpos[nex];
+    }
   }
-  cout << u << ' ' << tot << '\n';
 }
 
 
 int main(){
 	cin.tie(0) -> sync_with_stdio(0);
+  mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
   cin >> n >> q;
   for(int i = 1; i<=n; i++){
     cin >> arr[i];
+    b[i] = (i - 1)/SQRT +1;
   }
-  for(int i = 0; i<q; i++){
+  recompute(1, n);
+  while(q--){
     int op; cin >> op;
     if(op == 0){
-      int a, b;
-      cin >> a >> b;
-      todo[ind++] = query(op, a, b);
-      qu[a] = 1;
+      int a, B;
+      cin >> a >> B;
+      arr[a] = B;
+      recompute(down(b[a]), up(b[a]));
     }else{
-      int a; 
-      cin >> a;
-      todo[ind++] = query(op, a);
-    }
-    if(ind == SQRT || i == q - 1){
-      recompute();
-      for(int j = 0; j<ind; j++){
-        if(todo[j].op == 0){
-          int a= todo[j].u, b = todo[j].val;
-          arr[a] = b;
-          par[a] = a + b;
-        }else{
-          Q(todo[j].u);
-        }
+      int a; cin >> a;
+      int p = a, l = a, j = 0;
+      while(p <= n){
+        j += jumps[p];
+        l = lpos[p];
+        p = par[p];
       }
-      ind = 0;
+      cout << l << " " << j << '\n';
     }
+
   }
 	return 0;
 }
