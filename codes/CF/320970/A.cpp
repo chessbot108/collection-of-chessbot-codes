3ba675fd172befbe575a0306a9c720c39e89f265
chessbot108 <weimingchess108@gmail.com>
2021-03-21 21:31:43 -0700

    A is a scam :/

---
+++ codes/CF/320970/A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <utility>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <chrono>
+
+#define cont continue
+#define pow2(n) (1 << (n))
+#define ll long long
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define lsb(n) ((n)&(-(n)))
+#define LC(n) (((n) << 1) + 1)
+#define RC(n) (((n) << 1) + 2)
+#define add(a, b) (((a)%mod + (b)%mod)%mod)
+#define mul(a, b) (((a)%mod * (b)%mod)%mod)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+//geniousity limit exceeded
+#define moo printf
+#define oom scanf
+const ll mod = 1e9 + 7;
+const int MX = 1e2 +10, int_max = 0x3f3f3f3f;
+using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+void setIO(const string& file_name){
+	freopen((file_name+".in").c_str(), "r", stdin);
+	freopen((file_name+".out").c_str(), "w+", stdout);
+}
+//stoned game
+int arr[MX], n, T;
+int main(){
+  cin.tie(0) -> sync_with_stdio(0);
+  cin >> T;
+  while(T--){
+    cin >> n; init(arr, 0);
+    int mx = 0, sum = 0;
+    for(int i = 0; i<n; i++){
+      cin >> arr[i]; 
+      mx = max(mx, arr[i]);
+      sum += arr[i];
+    }
+    if(sum%2 == 0 && mx <= sum/2) moo("HL\n");
+    else moo("T\n");
+  }
+	return 0;
+}
+
+
