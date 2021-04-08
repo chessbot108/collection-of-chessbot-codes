bcf991c4878d313f5a81e92f59ff039c4ea084c2
chessbot108 <weimingchess108@gmail.com>
2021-04-07 22:29:09 -0700

    99 cses

---
+++ codes/CSES/CSES_1073.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <utility>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <queue>
+#include <set>
+
+#define cont continue
+#define pow2(n) (1 << (n))
+#define ll long long
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define ins insert
+#define lsb(n) ((n)&(-(n)))
+#define LC(n) (((n) << 1) + 1)
+#define RC(n) (((n) << 1) + 2)
+#define add(a, b) (((a)%mod + (b)%mod)%mod)
+#define mul(a, b) (((a)%mod * (b)%mod)%mod)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+#define tern(a, b, c) ((a) ? (b) : (c))
+//the following ones give off usaco vibes
+#define pbenq priority_queue
+#define moo printf
+#define oom scanf
+#define mool puts("") 
+#define loom getline
+
+const ll mod = 1e9 + 7;
+const int MX = 2e5 +10, int_max = 0x3f3f3f3f;
+
+using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+void setIO(const string& file_name){
+	freopen((file_name+".in").c_str(), "r", stdin);
+	freopen((file_name+".out").c_str(), "w+", stdout);
+}
+
+multiset<int> ms;
+int n, arr[MX];
+
+
+int main(){
+  cin.tie(0) -> sync_with_stdio(0);
+  cin >> n;
+  for(int i = 0; i<n; i++){
+    cin >> arr[i];
+  }
+
+  for(int i = 0; i<n; i++){
+    auto it = ms.lower_bound(arr[i] + 1); //does this work?
+    if(it != ms.end()) ms.erase(it);
+    ms.ins(arr[i]);
+  }
+  moo("%d\n", ms.size());
+	return 0;
+}
+
+
---
+++ codes/SPOJ/SPOJ_HAYBALE.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <utility>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <queue>
+#define cont continue
+
+#define pow2(n) (1 << (n))
+#define ll long long
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define ins insert
+#define lsb(n) ((n)&(-(n)))
+#define LC(n) (((n) << 1) + 1)
+#define RC(n) (((n) << 1) + 2)
+#define add(a, b) (((a)%mod + (b)%mod)%mod)
+#define mul(a, b) (((a)%mod * (b)%mod)%mod)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+#define tern(a, b, c) ((a) ? (b) : (c))
+//the following ones give off usaco vibes
+#define pbenq priority_queue
+#define moo printf
+#define oom scanf
+#define mool puts("") 
+#define loom getline
+
+const ll mod = 1e9 + 7;
+const int MX = 1e6 +10, int_max = 0x3f3f3f3f;
+
+using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+void setIO(const string& file_name){
+	freopen((file_name+".in").c_str(), "r", stdin);
+	freopen((file_name+".out").c_str(), "w+", stdout);
+}
+
+int pre[MX], arr[MX], n, q;
+
+int main(){
+  cin.tie(0) -> sync_with_stdio(0);
+  cin >> n >> q;
+  while(q--){
+    int a, b; cin >> a >> b;
+    pre[a]++; pre[b + 1]--;
+  }
+  for(int i = 1; i<=n; i++){
+    pre[i] += pre[i - 1];
+    //moo("%d\n", pre[i]);
+    assert(pre[i] >= 0);
+    arr[i] = pre[i];
+  }
+  sort(arr + 1, arr + n + 1);
+  moo("%d\n", arr[n/2 + 1]);
+	return 0;
+}
+
+
