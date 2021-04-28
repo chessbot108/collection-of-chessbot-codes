7601b7bbb7e5c5a805c8da66f1e66339915bdcd6
chessbot108 <weimingchess108@gmail.com>
2021-04-27 21:42:17 -0700

    updated template to be friendly with doubles. deleted setio

--- codes/templates/skeleton.cpp
+++ codes/templates/skeleton.cpp
@@ -1,4 +1,4 @@
-
+//here take a cat
 #include <iostream>
 #include <cstdio>
 #include <cstring>
@@ -14,21 +14,23 @@
 #include <queue>
 #include <set>
 
-#define cont continue
-#define pow2(n) (1 << (n))
 #define ll long long
+#define lb long double
 #define pii pair<int, int>
 #define pb push_back
 #define mp make_pair
 #define ins insert
+#define cont continue
+#define pow2(n) (1 << (n))
 
 #define LC(n) (((n) << 1) + 1)
 #define RC(n) (((n) << 1) + 2)
 #define add(a, b) (((a)%mod + (b)%mod)%mod)
 #define mul(a, b) (((a)%mod * (b)%mod)%mod)
-#define init(arr, val) memset(arr, val, sizeof(arr))
+#define sof(arr) sizeof(arr) //alternative for flexible memset?
 #define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
 #define tern(a, b, c) ((a) ? (b) : (c))
+#define feq(a, b) (fabs(a - b) < eps)
 
 #define pbenq priority_queue
 #define moo printf
@@ -37,15 +39,12 @@
 #define loom getline
 #define orz assert
 
+const lb eps = 1e-9;
 const ll mod = 1e9 + 7;
 const int MX = 2e5 +10, int_max = 0x3f3f3f3f;
 
 using namespace std;
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-void setIO(const string& file_name){
-  freopen((file_name+".in").c_str(), "r", stdin);
-  freopen((file_name+".out").c_str(), "w+", stdout);
-}
 
 int main(){
   cin.tie(0) -> sync_with_stdio(0);
