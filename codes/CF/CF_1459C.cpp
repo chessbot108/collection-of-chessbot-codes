91ad313a93d98288b3847b8ccae74f9da49bd783
chessbot108 <weimingchess108@gmail.com>
2021-05-10 10:04:54 -0700

    maffs

---
+++ codes/CF/CF_1459C.cpp
@@ -0,0 +1,72 @@
+//here take a cat
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <utility>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <queue>
+#include <set>
+
+#define ll long long
+#define lb long double
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define ins insert
+#define cont continue
+#define pow2(n) (1 << (n))
+
+#define LC(n) (((n) << 1) + 1)
+#define RC(n) (((n) << 1) + 2)
+#define add(a, b) (((a)%mod + (b)%mod)%mod)
+#define mul(a, b) (((a)%mod * (b)%mod)%mod)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+#define bckt(arr, val, sz) memset(arr, val, sizeof(arr[0]) * (sz))
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+#define tern(a, b, c) ((a) ? (b) : (c))
+#define feq(a, b) (fabs(a - b) < eps)
+
+#define moo printf
+#define oom scanf
+#define mool puts("") 
+#define orz assert
+#define fll fflush(stdout)
+
+const lb eps = 1e-9;
+const ll mod = 1e9 + 7, ll_max = (ll)1e18;
+const int MX = 2e5 +10, int_max = 0x3f3f3f3f;
+
+using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+ll n, m, a, d;
+ll gcd(ll x, ll y){
+  if(x < y) swap(x, y);
+  return tern(!y, x, gcd(y, x%y));
+} 
+
+
+int main(){
+  cin.tie(0) -> sync_with_stdio(0);
+  cin >> n >> m >> a;
+  for(int i = 0; i<n-1; i++){
+    ll k; cin >> k;
+    d = gcd(tern(i, d, 0), max(a, k) - min(a, k));
+  }
+  //moo("%lld %lld\n", a, d);
+  while(m--){
+    ll b; cin >> b;
+    moo("%lld\n", gcd(a + b, d));
+  }
+
+  return 0;
+}
+
+
