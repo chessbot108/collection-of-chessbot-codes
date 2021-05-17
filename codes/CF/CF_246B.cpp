11cb805fa0791f24d580ae6953f693dd46f995ed
chessbot108 <weimingchess108@gmail.com>
2021-05-17 11:43:57 -0700

    dont ask

---
+++ codes/CF/CF_246B.cpp
@@ -0,0 +1,58 @@
+//gyrating cat enthusiast
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <utility>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <queue>
+#include <set>
+
+#define ll long long
+#define lb long double
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define ins insert
+#define cont continue
+#define siz(vec) ((int)(vec.size()))
+
+#define LC(n) (((n) << 1) + 1)
+#define RC(n) (((n) << 1) + 2)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+#define bckt(arr, val, sz) memset(arr, val, sizeof(arr[0]) * (sz))
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+#define tern(a, b, c) ((a) ? (b) : (c))
+#define feq(a, b) (fabs(a - b) < eps)
+
+#define moo printf
+#define oom scanf
+#define mool puts("") 
+#define orz assert
+#define fll fflush(stdout)
+
+const lb eps = 1e-9;
+const ll mod = 1e9 + 7, ll_max = (ll)1e18;
+const int MX = 2e5 +10, int_max = 0x3f3f3f3f;
+
+using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int main(){
+  cin.tie(0) -> sync_with_stdio(0);
+  int n, tot = 0; cin >> n;
+  for(int i = 0; i<n; i++){
+    int a; cin >> a;
+    tot += a;
+  }
+  moo("%d\n", tern(tot%n, n-1, n));
+  return 0;
+}
+
+
