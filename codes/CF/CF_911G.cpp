95366a082e9b5ee94a16e0568ac0c08c51cab951
chessbot108 <weimingchess108@gmail.com>
2021-05-17 22:02:18 -0700

    cf

---
+++ codes/CF/CF_911G.cpp
@@ -0,0 +1,66 @@
+//gyrating cat enthusiast
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <utility>
+#include <cmath>
+#include <cassert>
+#include <algorithm>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <queue>
+#include <set>
+
+#define ll long long
+#define lb long double
+#define pii pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define ins insert
+#define cont continue
+#define siz(vec) ((int)(vec.size()))
+
+#define LC(n) (((n) << 1) + 1)
+#define RC(n) (((n) << 1) + 2)
+#define init(arr, val) memset(arr, val, sizeof(arr))
+#define bckt(arr, val, sz) memset(arr, val, sizeof(arr[0]) * (sz))
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+#define tern(a, b, c) ((a) ? (b) : (c))
+#define feq(a, b) (fabs(a - b) < eps)
+
+#define moo printf
+#define oom scanf
+#define mool puts("") 
+#define orz assert
+#define fll fflush(stdout)
+
+const lb eps = 1e-9;
+const ll mod = 1e9 + 7, ll_max = (ll)1e18;
+const int MX = 2e5 +10, int_max = 0x3f3f3f3f;
+
+using namespace std;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+unsigned char arr[MX], x, y;
+int n, q, a, b;
+
+int main(){
+  cin.tie(0) -> sync_with_stdio(0);
+  oom("%d", &n);
+  for(int i = 0; i<n; i++) oom("%hhu", &arr[i]);
+  oom("%d", &q);
+  while(q--){
+    oom("%d%d%hhu%hhu\n", &a, &b, &x, &y);
+    for(int i = a-1; i<b; i++) arr[i] = tern(arr[i] == x, y, arr[i]);
+  }
+  for(int i = 0; i<n; i++) moo("%hhu ", arr[i]); mool;
+  return 0;
+}
+
+
